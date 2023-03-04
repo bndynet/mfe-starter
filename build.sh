b375902522b55b95b42e62a7806f64236dd5f30d
Bendy Zhang <zb@bndy.net>
2023-03-04 10:03:36 +0800

    chore(ci): github actions for building

--- build.sh
+++ build.sh
@@ -5,9 +5,13 @@ rm -Rf app-react/build &
 rm -Rf app-vue/dist &
 wait
 
+npm i --prefix host &&
 npm run build --prefix host &
+npm i --prefix app-angular &&
 npm run build --prefix app-angular &
+npm i --prefix app-react && 
 PUBLIC_URL=/app-react/ npm run build --prefix app-react &
+npm i --prefix app-vue && 
 PUBLIC_URL=/app-vue/ npm run build --prefix app-vue &
 wait
 
